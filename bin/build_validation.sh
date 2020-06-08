49dfbc34bd0734cfeb0290aeca4da6d2e6137651
Denis Polom <dpolom@suse.de>
2020-06-08 13:01:05 +0200

    revert to 2bab7dc

--- bin/build_validation.sh
+++ bin/build_validation.sh
@@ -218,24 +218,6 @@ function destroy_on_aarch64 () {
     vagrant destroy -f
 }
 
-function destroy_on_intel () {
-    nodes_list=($(vagrant status | awk '/libvirt/{print $1}'))
-    virsh list --all --name | grep ${project}_ | xargs -I {} virsh destroy {}
-    rm -f ${qemu_default_pool}/${project}_*
-    systemctl restart libvirtd
-    for node in ${nodes_list[@]}
-    do
-        for snap in $(virsh snapshot-list --name ${project}_${node})
-        do
-            virsh snapshot-delete ${project}_${node} $snap
-        done
-    done
-
-    virsh list --all --name | grep ${project}_ | xargs -I {} virsh undefine {}
-
-    vagrant destroy -f
-}
-
 ses_deploy_scripts=(deploy_ses.sh hosts_file_correction.sh configure_ses.sh)
 project=$(basename $PWD)
 if $no_clients; then
@@ -290,7 +272,7 @@ EOF
 
 ### destroy existing cluster
 if $destroy && [ "$(arch)" == "x86_64" ];then
-    destroy_on_intel
+    vagrant destroy -f
     exit
 elif $destroy && [ "$(arch)" == "aarch64" ];then
     destroy_on_aarch64
@@ -381,7 +363,7 @@ fi
 
 ### destroy existing cluster before deploy (useful for Jenkins)
 if $destroy_b4_deploy && [ "$(arch)" == "x86_64" ];then
-    destroy_on_intel
+    vagrant destroy -f
 elif $destroy_b4_deploy && [ "$(arch)" == "aarch64" ];then
     destroy_on_aarch64
 fi
@@ -434,206 +416,6 @@ fi
 ### exit if SES only is required 
 ### or if only Salt cluster is required
 if $ses_only && $only_salt_cluster && ! $all_scripts && ! $only_script;then
-    exit
-fi
-ses_deploy_scripts=(deploy_ses.sh hosts_file_correction.sh configure_ses.sh)
-project=$(basename $PWD)
-if $no_clients; then
-    scripts=$(find scripts/ -maxdepth 1 -type f ! -name ${ses_deploy_scripts[0]} \
-         -and ! -name ${ses_deploy_scripts[1]} -and ! -name ${ses_deploy_scripts[2]} \
-         -and ! -name clients_\* -exec basename {} \;)
-else
-    scripts=$(find scripts/ -maxdepth 1 -type f ! -name ${ses_deploy_scripts[0]} \
-         -and ! -name ${ses_deploy_scripts[1]} -and ! -name ${ses_deploy_scripts[2]} \
-         -exec basename {} \;)
-fi
-ssh_options="-i ~/.ssh/storage-automation -l root -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-qemu_default_pool="$(virsh pool-dumpxml default | grep path | sed 's/<.path>//; s/<path>//')"
-libvirt_default_ip="$(virsh net-dumpxml default | awk '/ip address/{print $2}' | cut -d = -f 2 | sed "s/'//g")"
-
-### Creates repo files
-cat << EOF > /srv/www/htdocs/current_os.repo
-[basesystem]
-name=basesystem
-type=rpm-md
-baseurl=$sle_url/Module-Basesystem/
-gpgcheck=0
-gpgkey=$sle_url/Module-Basesystem/repodata/repomd.xml.key
-enabled=1
-
-[server-applications]
-name=server-applications
-type=rpm-md
-baseurl=$sle_url/Module-Server-Applications/
-gpgcheck=0
-gpgkey=$sle_url/Module-Server-Applications/repodata/repomd.xml.key
-enabled=1
-
-[product-sles]
-name=product-sles
-type=rpm-md
-baseurl=$sle_url/Product-SLES/
-gpgcheck=0
-gpgkey=$sle_url/Product-SLES/repodata/repomd.xml.key
-enabled=1
-EOF
-
-cat << EOF > /srv/www/htdocs/current_ses.repo
-[SES]
-name=SES
-type=rpm-md
-baseurl=$ses_url
-gpgcheck=0
-gpgkey=$ses_url/repodata/repomd.xml.key
-enabled=1
-EOF
-
-### destroy existing cluster
-if $destroy && [ "$(arch)" == "x86_64" ];then
-    vagrant destroy -f
-    exit
-elif $destroy && [ "$(arch)" == "aarch64" ];then
-    destroy_on_aarch64
-    exit
-fi
-
-### creates vagrnat box from SLP repo if box not already exists
-new_vagrant_box="${sle_slp_dir,,}"
-new_vagrant_box="${new_vagrant_box//-/}"
-if [ -z "$vagrant_box" ] && [ -z "$(vagrant box list | grep -w $new_vagrant_box)" ];then
-    if [ -z "$sle_slp_dir" ] || [ -z "$ses_slp_dir" ];then
-        echo "missing --sle_slp_dir or --ses_slp_dir"
-        exit 1
-    fi
-    
-    if [ "$(arch)" == "x86_64" ]; then
-        if [ -f "/srv/www/htdocs/autoyast_intel.xml" ];then
-            mv  /srv/www/htdocs/autoyast_intel.xml /srv/www/htdocs/autoyast_intel.xml.$(date +%F-%H%M)
-        fi
-        cp $(dirname $(realpath $0))/../autoyast/autoyast_intel.xml /srv/www/htdocs/autoyast_intel.xml
-        sed -i "s/REPLACE_ME/${sle_url//\//\\/}/g" /srv/www/htdocs/autoyast_intel.xml
-
-        virt-install --name vgrbox --memory 2048 --vcpus 1 --hvm \
-        --disk bus=virtio,path=/qemu/pools/default/vgrbox.qcow2,cache=none,format=qcow2,size=10  \
-        --network bridge=virbr0,model=virtio --connect qemu:///system  --os-type linux \
-        --os-variant sle15sp2 --virt-type kvm --noautoconsole --accelerate \
-        --location $sle_url \
-        --extra-args="console=tty0 console=ttyS0,115200n8 autoyast=http://$libvirt_default_ip/autoyast_intel.xml"
-    elif [ "$(arch)" == "aarch64" ];then
-        if [ -f "/srv/www/htdocs/autoyast_aarch64.xml" ];then
-            mv  /srv/www/htdocs/autoyast_aarch64.xml /srv/www/htdocs/autoyast_aarch64.xml.$(date +%F-%H%M)
-        fi
-        cp $(dirname $(realpath $0))/../autoyast/autoyast_aarch64.xml /srv/www/htdocs/autoyast_aarch64.xml
-        sed -i "s/REPLACE_ME/${sle_url//\//\\/}/g" /srv/www/htdocs/autoyast_aarch64.xml
-
-        virt-install --name vgrbox --memory 2048 --vcpus 1 --hvm \
-        --disk bus=virtio,path=/qemu/pools/default/vgrbox.qcow2,cache=none,format=qcow2,size=10  \
-        --network bridge=virbr0,model=virtio --connect qemu:///system  --os-type linux \
-        --os-variant sle15sp2 --arch aarch64 --noautoconsole --accelerate \
-        --location $sle_url \
-        --extra-args="console=ttyAMA0,115200n8 autoyast=http://$libvirt_default_ip/autoyast_aarch64.xml"
-    fi
-    
-    echo
-    echo "Waiting till vgrbox installation finish"
-    while [ "$(virsh domstate vgrbox)" != "shut off" ];do sleep 60;done
-    
-    echo 
-    echo "Starting vgrbox for 2nd stage"
-    virsh start vgrbox
-    
-    sleep 10
-    
-    echo
-    echo "Waiting till vgrbox 2nd stage installation finish"
-    while [ "$(virsh domstate vgrbox)" != "shut off" ];do sleep 60;done
-    
-    vagrant_box="$new_vagrant_box"
-    echo "creating vagrant box $vagrant_box"
-    
-    mkdir -p $VAGRANT_HOME/boxes/$vagrant_box/0/libvirt || exit 1
-    
-    metadata_json
-    vgrvagrantfile
-    
-    mv $qemu_default_pool/vgrbox.qcow2 $VAGRANT_HOME/boxes/$vagrant_box/0/libvirt/box.img
-    
-    if [ "$(arch)" == "x86_64" ];then
-        virsh undefine vgrbox
-    elif [ "$(arch)" == "aarch64" ];then
-        virsh undefine vgrbox --nvram
-    fi
-
-    
-    if [ "$(vagrant box list | grep -w $vagrant_box )" ]; then
-        echo "vagrant box $vagrant_box created"; else
-        exit 1
-    fi
-    
-    ln -s $VAGRANT_HOME/boxes/$vagrant_box/0/libvirt/box.img $qemu_default_pool/${vagrant_box}_vagrant_box_image_0.img
-    
-    systemctl restart libvirtd
-    
-    rm -f /srv/www/htdocs/autoyast_{intel,aarch64}.xml
-else
-    vagrant_box="$new_vagrant_box"
-fi
-
-### destroy existing cluster before deploy (useful for Jenkins)
-if $destroy_b4_deploy && [ "$(arch)" == "x86_64" ];then
-    vagrant destroy -f
-elif $destroy_b4_deploy && [ "$(arch)" == "aarch64" ];then
-    destroy_on_aarch64
-fi
-
-### creates nodes and deploys SES 
-if ! $existing
-then
-    if ! $only_salt_cluster
-    then
-        sed -i 's/deploy_ses: .*/deploy_ses: true/' ${VAGRANT_VAGRANTFILE}.yaml
-    else
-        sed -i 's/deploy_ses: .*/deploy_ses: false/' ${VAGRANT_VAGRANTFILE}.yaml
-    fi
-
-    if [ -z "$vagrant_box" ]
-    then
-        echo "Missing --vagrant-box-name parameter"
-    else
-        sed -i "s/ses_cl_box: .*/ses_cl_box: $vagrant_box/" ${VAGRANT_VAGRANTFILE}.yaml
-    fi
-
-    vagrant up 
-
-    if [ "$(arch)" == "aarch64" ];then
-        rsync -aP --delete /etc/libvirt/qemu/ /etc/libvirt/qemu_pflash
-    fi
-    
-    if [ $? -ne 0 ];then exit 1;fi
- 
-    set_variables
-
-    nodes_list=($(vagrant status | awk '/libvirt/{print $1}'))
-    
-    vssh_script "${monitors[0]}" "configure_ses.sh" 
-    
-    create_snapshot "$(echo ${nodes_list[@]})" "deployment"
-    
-    for node in ${nodes_list[@]}
-    do
-        virsh start ${project}_${node}
-    done
-    
-    wait_for_health_ok
-
-else
-    set_variables
-fi
-    
-
-### exit if SES only is required 
-### or if only Salt cluster is required
-if $ses_only &&  $only_salt_cluster || ! $all_scripts && ! $only_script;then
     exit
 fi
 
