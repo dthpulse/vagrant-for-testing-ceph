12581a83637bef1816211e703f3dd5ee2b4baaee
Denis Polom <dpolom@suse.de>
2020-06-10 13:51:26 +0200

    new tests

---
+++ scripts/SES7/CephFS_test-mount_CephFS.sh
@@ -0,0 +1,29 @@
+set -ex
+
+monitors=($monitors)
+master=$master
+
+random_minion_fqdn=${monitors[0]}
+
+ceph fs volume create myfs ${random_minion_fqdn%%.*}
+
+while [ "$(ceph fs ls --format json | jq -r .[].name)" != "myfs" ]
+do
+    sleep 5
+done
+
+secret=$(grep key /etc/ceph/ceph.client.admin.keyring | sed 's/key\ =\ //')
+mount_monitors="$(echo "${monitors[@]}" | tr ' ' ',')"
+
+mkdir /mnt/cephfs
+
+mount -t ceph $mount_monitors:/ /mnt/cephfs -o name=admin,secret=$(echo $secret | tr -d ' ')
+
+mount | grep "/mnt/cephfs"
+
+dd if=/dev/zero of=/mnt/cephfs/testfile.bin oflag=direct bs=1M count=100 status=progress
+
+rm -f /mnt/cephfs/testfile.bin
+
+umount /mnt/cephfs
+rm -rf /mnt/cephfs
