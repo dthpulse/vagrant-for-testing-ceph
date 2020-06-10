8652f43446196d1e23c0956c12905954a460323c
Denis Polom <dpolom@suse.de>
2020-06-10 19:08:30 +0200

    fix

--- scripts/SES7/rgw_basic.sh
+++ scripts/SES7/rgw_basic.sh
@@ -12,12 +12,12 @@ function test_rgw {
     local action="$1"
     local realm_name="$2"
     local zone_name="$3"
-    local daemon_name="$(ceph orch ps --daemon_type rgw --format json | jq -r '.[].daemon_id')"
-    local service_name="ceph-${ceph_fsid}@rgw.${daemon_name}"
     ceph orch $action rgw --realm_name=$realm_name --zone_name=$zone_name 1 ${monitor_rgw%%.*}:$monitor_rgw_ip
     while [ -z "$(ceph orch ps | awk '/rgw/&&/running/{print $0}')" ];do
         sleep 30
     done
+    local daemon_name="$(ceph orch ps --daemon_type rgw --format json | jq -r '.[].daemon_id')"
+    local service_name="ceph-${ceph_fsid}@rgw.${daemon_name}"
     ssh $monitor_rgw "systemctl is-active $service_name"
     ssh $monitor_rgw "systemctl restart $service_name"
     sleep 10
