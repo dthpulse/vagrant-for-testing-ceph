be396dfaa6de2839c798d763d3db324e450bae05
Denis Polom <dpolom@suse.de>
2020-07-16 15:41:53 +0200

    force_password_update disable

--- scripts/SES7/deploy_ses.sh
+++ scripts/SES7/deploy_ses.sh
@@ -33,6 +33,7 @@ ceph-salt config /time_server/server_hostname set "$master"
 ceph-salt config /time_server/external_servers add "ntp.suse.cz"
 ceph-salt config /cephadm_bootstrap/dashboard/username set admin
 ceph-salt config /cephadm_bootstrap/dashboard/password set admin
+ceph-salt config /cephadm_bootstrap/dashboard/force_password_update disable
 
 if [ "$(arch)" == "x86_64" ];then
     ceph-salt config /containers/registries add prefix=registry.suse.de location=192.168.122.1:5000 insecure=true
