ddaf063fd6906ba8e0f3c73dd403b3ec6fc02ffb
Denis Polom <dpolom@suse.de>
2020-07-16 17:17:09 +0200

    deploy RGW using spec file

--- scripts/SES7/rgw_basic.sh
+++ scripts/SES7/rgw_basic.sh
@@ -1,41 +1,30 @@
 set -ex
 
-monitors_all=($monitors)
-monitors=(${monitors_all[@]/$(hostname -f)})
+master=$master
+monitors=($monitors)
+osd_nodes=($osd_nodes)
 
-monitor_rgw=${monitors[0]}
-monitor_rgw_ip=$(ssh $monitor_rgw "hostname -i")
-ceph_fsid=$(ceph fsid)
+radosgw-admin realm create --rgw-realm=realm1 --default
+
+radosgw-admin zonegroup create --rgw-zonegroup=zonegroup1 \
+    --master --default
+
+radosgw-admin zone create --rgw-zonegroup=zonegroup1 \
+    --rgw-zone=zone1 --master --default
 
 
-function test_rgw {
-    local action="$1"
-    local realm_name="$2"
-    local zone_name="$3"
-    ceph orch $action rgw --realm_name=$realm_name --zone_name=$zone_name 1 ${monitor_rgw%%.*}:$monitor_rgw_ip
-    while [ -z "$(ceph orch ps | awk '/rgw/&&/running/{print $0}')" ];do
-        sleep 30
-    done
-    local daemon_name="$(ceph orch ps --daemon_type rgw --format json | jq -r '.[].daemon_id')"
-    local service_name="ceph-${ceph_fsid}@rgw.${daemon_name}"
-    ssh $monitor_rgw "systemctl is-active $service_name"
-    ssh $monitor_rgw "systemctl restart $service_name"
-    sleep 10
-    ssh $monitor_rgw "systemctl is-active $service_name" 
-    ssh $monitor_rgw "systemctl stop $service_name"
-    sleep 5
-    ssh $monitor_rgw "systemctl start $service_name"
-    sleep 10
-    ssh $monitor_rgw "systemctl is-active $service_name"
-    radosgw-admin zone list --format json | jq -r .zones[] | grep $zone_name
-    #radosgw-admin realm list --format json | jq -r .realms[] | grep $realm_name
-}
+cat << EOF > /tmp/rgw.yaml
+service_type: rgw
+service_id: realm1.zone1
+placement:
+ hosts:
+  - ${osd_nodes[0]%%.*}
+  - ${osd_nodes[1]%%.*}
+  - ${osd_nodes[2]%%.*}
+EOF
 
-# creates rgw
-test_rgw apply default default
+ceph orch apply -i /tmp/rgw.yaml
 
-## change realm
-#test_rgw update default realm1
-#
-## change zone
-#test_rgw update zone1 realm1
+sleep 60
+
+ceph orch ps --refresh --daemon_type rgw | grep rgw
