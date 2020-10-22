84e808a2595fbdc77fe4004723b244a801e5702f
Denis Polom <dpolom@suse.de>
2020-10-22 14:23:20 +0200

    M20

--- scripts/SES7/deploy_ses.sh
+++ scripts/SES7/deploy_ses.sh
@@ -8,9 +8,13 @@ systemctl restart salt-master
 if [ "$(arch)" == "x86_64" ]
 then
     sleep 240
+    ntp_server="ntp.suse.cz"
+    registries="add prefix=registry.suse.de location=192.168.122.1:5000 insecure=true"
 elif [ "$(arch)" == "aarch64" ]
 then
     sleep 660
+    ntp_server="ntp.suse.de"
+    registries=""
 fi
 
 salt \* saltutil.sync_all
@@ -31,15 +35,11 @@ done
 ceph-salt config /ssh generate
 ceph-salt config /time_server/servers add "$master"
 ceph-salt config /time_server/subnet set "192.168.122.0/24"
-ceph-salt config /time_server/external_servers add "ntp.suse.cz"
+ceph-salt config /time_server/external_servers add "$ntp_server"
 ceph-salt config /cephadm_bootstrap/dashboard/username set admin
 ceph-salt config /cephadm_bootstrap/dashboard/password set admin
 ceph-salt config /cephadm_bootstrap/dashboard/force_password_update disable
-
-if [ "$(arch)" == "x86_64" ];then
-    ceph-salt config /containers/registries_conf/registries add prefix=registry.suse.de location=192.168.122.1:5000 insecure=true
-fi
-
+ceph-salt config /containers/registries_conf/registries $registries
 ceph-salt config /cephadm_bootstrap/ceph_image_path set "registry.suse.de/suse/sle-15-sp2/update/products/ses7/update/cr/containers/ses/7/ceph/ceph"
 
 ceph-salt config ls
